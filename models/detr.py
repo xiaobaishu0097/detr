81a2fc09c7edf17aa13b1fe4d51dcb562dc53a6d
xiaobaishu0097 <du_heming@hotmail.com>
2020-07-18 22:30:02 +1000

    temporarily removing some segmentation labels during training detection; adding supports to the AI2Thor_Det_21cls_Data

--- models/detr.py
+++ models/detr.py
@@ -306,7 +306,7 @@ def build(args):
     if args.dataset_file == "coco_panoptic":
         num_classes = 250
     if args.dataset_file == 'AI2Thor_Det_21cls_Data':
-        num_classes = 21
+        num_classes = 22
     device = torch.device(args.device)
 
     backbone = build_backbone(args)
